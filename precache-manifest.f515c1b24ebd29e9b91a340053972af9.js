e9ae1296b5e4554226df49124683831921fa3ed4
Kevin Su <kevinsu@outlook.co.nz>
2020-03-02 13:04:22 +1100

    Updates

---
+++ precache-manifest.f515c1b24ebd29e9b91a340053972af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c203d7e94128688f6985d94fec44a7",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "75fa96ed9f2d06cf6cd6",
+    "url": "/reactapp/static/css/main.9d8339a7.chunk.css"
+  },
+  {
+    "revision": "1911b28e5457b0728dfa",
+    "url": "/reactapp/static/js/2.8b6d70ee.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/reactapp/static/js/2.8b6d70ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75fa96ed9f2d06cf6cd6",
+    "url": "/reactapp/static/js/main.55eb1e91.chunk.js"
+  },
+  {
+    "revision": "58693318a4ea0ce1a6c3",
+    "url": "/reactapp/static/js/runtime-main.8c357848.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
